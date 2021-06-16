c7beb3253752bc4ecb49a1e79246fd1526407dd5
Jose Alanya <jose.alanya@gmail.com>
2021-06-15 19:34:39 -0500

    FEAT: Implementing Multi-Tier Navigation: Syncing History Objects

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,13 +1,18 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
     mount(ref.current, {
-      onNavigate: () => {
-        console.log('The container noticed navigation')
+      onNavigate: ({ pathname: nextPathName }) => {
+        const { pathname } = history.location;
+        if (pathname !== nextPathName) {
+          history.push(nextPathName);
+        }
       }
     });
   });
