6ef7971b6fd08f276136bc8b9a73c54f1048f500
Jose Alanya <jose.alanya@gmail.com>
2021-06-17 16:55:01 -0500

    FEAT: Performance Considerations: Adding Signin and Signup Forms

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
+import Signin from './components/Signin';
+import Singup from './components/Signup';
+
 const generateClassName = createGenerateClassName({
   productionPrefix: 'au',
 });
@@ -10,7 +13,10 @@ export default ({ history }) => {
   return <div>
     <StylesProvider generateClassName={generateClassName}>
       <Router history={history}>
-        <Switch></Switch>
+        <Switch>
+          <Route path="/auth/signin" component={Signin}></Route>
+          <Route path="/auth/signup" component={Singup}></Route>
+        </Switch>
       </Router>
     </StylesProvider>
   </div>
