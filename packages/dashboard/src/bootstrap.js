270c45b711bda6dc9d5dd40895d6397968d24ff8
Jose Alanya <jose.alanya@gmail.com>
2021-09-02 18:57:24 -0500

    FEAT: Using Other Frontend Frameworks: Initial Dashboard Setup using Vue

---
+++ packages/dashboard/src/bootstrap.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+import App from './App';
+
+// Mount function to start up th app
+const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
+  const history = defaultHistory || createMemoryHistory({ initialEntries: [initialPath]});
+  onNavigate && history.listen(onNavigate);
+  ReactDOM.render(
+    <App onSignIn={onSignIn} history={history} />,
+    el
+  );
+
+  return {
+    onParentNavigate({ pathname: nextPathName }) {
+      const { pathname } = history.location;
+      console.log(nextPathName);
+      if (pathname !== nextPathName) {
+        history.push(nextPathName);
+      }
+    }
+  }
+};
+
+// If we are in deployment and in isolation,
+// call mount immediately
+
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_dashboard-dev-root');
+  if (devRoot) {
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+// We are running through container
+// and we should export the mount function
+export { mount };
